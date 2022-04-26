2046a9a990103d2408304671bd0734541f3c94d7
Axel Jacobsen <aejacobs@stud.ntnu.no>
2022-04-26 03:45:41 +0200

    Added simple character init, needs read from file

---
+++ src/InitializeCharacter.hs
@@ -0,0 +1,21 @@
+module InitializeCharacter (generateCharacter) where
+import Structs (Player (..))
+
+generateCharacter :: String -> String -> Player
+generateCharacter "" name = Player { 
+    name = name,
+    hp=10, 
+    weapon  = 0, 
+    armour  = 0, 
+    healpot = 1, 
+    money   = 10, 
+    lowestLayer = 0}
+
+generateCharacter fileData name = Player { 
+    name = name,
+    hp=10, 
+    weapon  = 0, 
+    armour  = 0, 
+    healpot = 1, 
+    money   = 10, 
+    lowestLayer = 0}

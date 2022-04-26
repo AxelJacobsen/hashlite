773c98011536b1f175db15c9e6f4918f0df7cee2
Axel Jacobsen <aejacobs@stud.ntnu.no>
2022-04-26 03:44:36 +0200

    Updated main to generate empty map and character

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,15 @@ module Main where
 
 import MapGenerator (generateBoard)
 import System.Random (mkStdGen, newStdGen)
+import Structs (Player)
+import InitializeCharacter (generateCharacter)
+import Ip_mainLoop (gameLoop)
+import System.IO
 
 main :: IO ()
 main = do
     originalSeed <- newStdGen
-    print $ generateBoard 10 6 originalSeed
+    --CHECK FILE FOR EXISTING PLAYER FIRST
+    
+    --IF NO PLAYER EXISTS ASK FOR NAME AND START
+    gameLoop (generateCharacter "" "") (generateBoard 10 5 originalSeed)

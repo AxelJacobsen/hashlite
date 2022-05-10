d9612e3e6e5035d167b3b154ccc42dcd8263167d
Axel Jacobsen <aejacobs@stud.ntnu.no>
2022-05-10 17:11:25 +0200

    Added functions to generate an empty map

--- src/initialization/pure/MapGenerator.hs
+++ src/initialization/pure/MapGenerator.hs
@@ -1,4 +1,4 @@
-module Initialization.Pure.MapGenerator (generateBoard) where 
+module Initialization.Pure.MapGenerator (generateBoard,generateEmptyBoard) where 
 
 import System.Random ( Random(randomR), StdGen )
 
@@ -19,3 +19,20 @@ fillInnerBoard' counter maxRngVal inSeed map = do
     let (randNum, outSeed) = randomR (0, maxRngVal) inSeed :: (Int, StdGen)
     if randNum <= 2 then fillInnerBoard' (counter-1) maxRngVal outSeed (map++[0])
     else fillInnerBoard' (counter-1) maxRngVal outSeed (map++[randNum])
+
+
+-- Generates empty board for navigation
+generateEmptyBoard :: Int -> [[Int]]
+generateEmptyBoard size = fillBoardEmpty' size size [[]]
+
+fillBoardEmpty' :: Int -> Int -> [[Int]] -> [[Int]]
+fillBoardEmpty' _ 0 map = map
+fillBoardEmpty' size count board = do
+    let mapCom = fillInnerBoardEmpty' size []
+    if board == [[]] then fillBoardEmpty' size (count-1) [mapCom] 
+    else fillBoardEmpty' size (count-1) (board++[mapCom])
+
+-- x <= 2 == Empty, 3 == combat, 4 == chest 5 == encounter
+fillInnerBoardEmpty' :: Int -> [Int] -> [Int]
+fillInnerBoardEmpty' 0 map = map
+fillInnerBoardEmpty' counter map = fillInnerBoardEmpty' (counter-1) (map++[-1])

3a6e1c9cceb1f2e9614245f86ad3db85db6c6854
Axel Jacobsen <aejacobs@stud.ntnu.no>
2022-05-10 23:16:01 +0200

    REmoved unecessary StdGen being sent in all funcs

--- src/initialization/pure/P_initChar.hs
+++ src/initialization/pure/P_initChar.hs
@@ -1,53 +1,56 @@
-module Initialization.Pure.P_initChar (generateCharacter, placeStartEnd) where
+module Initialization.Pure.P_initChar (generateCharacter, placeStartEnd, setMarkerEntry) where
 import Structs (Player (..))
 import System.Random ( Random(randomR), StdGen )
 import Public.P_publicFuncs(drop')
+import Public.P_updatePlayer (updatePos)
 
 generateCharacter :: String -> Player
 generateCharacter name = Player {
     name = name,
+    maxHp=10,
     hp=10,
     weapon  = 0,
     armour  = 0,
     healpot = 1,
     money   = 10,
-    lowestLayer = 0}
+    lowestLayer = 0,
+    playerPos = (0,0),
+    goal = (0,0)}
 
 --Handles getting limits for positioning
-placeStartEnd :: Int -> Bool -> ([[Int]],StdGen) -> ([[Int]],StdGen)
+placeStartEnd :: Int -> Bool -> ([[Int]],StdGen) -> ([[Int]], StdGen, Int,Int)
 placeStartEnd size isVisible (map, inseed) = do
     let randomLower = calcLower size 0.25
-    let (startX, seedOne) = randomR (randomLower, size) inseed :: (Int, StdGen) --Generates a starting position
-    let (startY, seedTwo) = randomR (randomLower, size) seedOne :: (Int, StdGen) --Generates a starting position
-    let (endX , seedThree) = randomR (0, size) seedTwo :: (Int, StdGen) --Generates a starting position
+    let (startX, seedOne) = randomR (randomLower, size) inseed :: (Int, StdGen) --Generates starting position X
+    let (startY, seedTwo) = randomR (randomLower, size) seedOne :: (Int, StdGen) --Generates starting position Y
+    let (endX , seedThree) = randomR (0, size) seedTwo :: (Int, StdGen) --Generates exit position X
     --let mapWithStart = setStartRec (startX, startY) map ([[]], outSeed)
-    let (mapWithStart, _) = setMarkerEntry (startX, startY) (size,size) 99 map ([[]], seedThree)
+    let mapWithStart = setMarkerEntry (startX, startY) (size,size) 99 map [[]]
 
-    if isVisible then (mapWithStart, inseed) 
+    if isVisible then (mapWithStart, inseed, startX, startY)
     else do
         if startY < size `div` 2 then do
-            let (endY , outSeed) = randomR (size `div` 2, size) seedThree :: (Int, StdGen)
-            setMarkerEntry (endX, endY) (size,size) 100 mapWithStart ([[]], outSeed)
+            let (endY, outSeed) = randomR (size `div` 2, size) seedThree :: (Int, StdGen)
+            (setMarkerEntry (endX, endY) (size,size) 100 mapWithStart [[]], outSeed, endX, endY)
         else do
-            let (endY , outSeed) = randomR (floor 0, size`div`2) seedThree :: (Int, StdGen)
-            setMarkerEntry (endX, endY) (size,size) 100 mapWithStart ([[]], outSeed)
+            let (endY, outSeed) = randomR (floor 0, size`div`2) seedThree :: (Int, StdGen)
+            (setMarkerEntry (endX, endY) (size,size) 100 mapWithStart [[]] , outSeed, endX, endY)
 
 --Inserts start and end marker into map 
-setMarkerEntry :: (Int,Int) -> (Int,Int) -> Int -> [[Int]] -> ([[Int]],StdGen) -> ([[Int]],StdGen)
-setMarkerEntry (_,_) (_,_) _ [[]]  (map,seed) = (map,seed) -- List has been iterated or was empty
-setMarkerEntry (_,_) (_,_) _ []  (map,seed) = (map,seed) -- List has been iterated or was empty
-setMarkerEntry (markX, markY) (sizeX, sizeY) marker (curMap:original) (map, keepSeed)
-    | markX == sizeX && sizeX == 0 && map == [[]] = ([setMarkerInner markY sizeY marker [] curMap], keepSeed)
-    | markX == sizeX && sizeX == 0 = (map++[setMarkerInner markY sizeY marker [] curMap], keepSeed)
-    | markX == sizeX = setMarkerEntry (markX, markY) (sizeX-1, sizeY) marker original (map++[setMarkerInner markY sizeY marker [] curMap], keepSeed)--setStartRec (startX, startY) (endX, endY) (map, keepSeed)
-    | map == [[]] = setMarkerEntry (markX, markY) (sizeX-1, sizeY) marker original ([curMap], keepSeed)
-    | otherwise = setMarkerEntry (markX, markY) (sizeX-1, sizeY) marker original (map++[curMap], keepSeed)
-
+setMarkerEntry :: (Int,Int) -> (Int,Int) -> Int -> [[Int]] -> [[Int]] -> [[Int]]
+setMarkerEntry (_,_) (_,_) _ [[]]  map = map -- List has been iterated or was empty
+setMarkerEntry (_,_) (_,_) _ []  map = map -- List has been iterated or was empty
+setMarkerEntry (markX, markY) (sizeX, sizeY) marker (curMap:original) map
+    | markX == sizeX && sizeX == 0 && map == [[]] = [setMarkerInner markY sizeY marker [] curMap]
+    | markX == sizeX && sizeX == 0 = map++[setMarkerInner markY sizeY marker [] curMap]
+    | markX == sizeX = setMarkerEntry (markX, markY) (sizeX-1, sizeY) marker original (map++[setMarkerInner markY sizeY marker [] curMap])--setStartRec (startX, startY) (endX, endY) (map, keepSeed)
+    | map == [[]] = setMarkerEntry (markX, markY) (sizeX-1, sizeY) marker original [curMap]
+    | otherwise = setMarkerEntry (markX, markY) (sizeX-1, sizeY) marker original (map++[curMap])
 
 --Replaces nth value safely
 setMarkerInner :: Int -> Int -> Int -> [Int] -> [Int] -> [Int]
 setMarkerInner _ _ _ outMap [] = outMap
-setMarkerInner markPos fullLength marker curMap (mapIt:inMap) 
+setMarkerInner markPos fullLength marker curMap (mapIt:inMap)
     | markPos == 0 && fullLength == 0 && curMap /= [] = curMap++[marker]
     | markPos == 0 && fullLength == 0 = [marker]
     | fullLength == 0 = curMap

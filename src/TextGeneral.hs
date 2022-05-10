75296be2110118b3c68780639fdf793a458d9452
Axel Jacobsen <aejacobs@stud.ntnu.no>
2022-05-10 23:15:30 +0200

    Added more text to text general

--- src/TextGeneral.hs
+++ src/TextGeneral.hs
@@ -1,8 +1,11 @@
-module TextGeneral (idleOptions,pressEnter,exitGame,enterName,moveOptions) where
+module TextGeneral (idleOptionsOne,idleOptionsTwo,pressEnter,exitGame,enterName,moveSymbols,moveOptions,moveIllegal) where
 -- General text
 
 pressEnter = "Welcome! press enter to start!"
 enterName = "Please enter a name for our Hero!:"
-idleOptions = "Do you want to:\n\tMove (m)\n\tRest (r)\n\tQuit (q)"
+idleOptionsOne = "What do you want to do "
+idleOptionsTwo = "\n\tMove (m)\n\tRest (r)\n\tQuit (q)"
 exitGame = "Are you sure you want to quit?\nAll progress will be lost (y/n): "
-moveOptions = "Moved path & Walls: | -\nStarting position: S\nPlayer: P"
+moveSymbols = "Moved path & Walls: | -\nStarting position: S\nPlayer: P"
+moveOptions = "Where do you want to walk? (w = up, a = left, s = down, d = right):"
+moveIllegal = "Illegal direction."

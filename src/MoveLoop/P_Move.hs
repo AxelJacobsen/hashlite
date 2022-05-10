1ade1354ab9b6b8b57b3d5816409601b39381cfc
Axel Jacobsen <aejacobs@stud.ntnu.no>
2022-05-10 23:14:45 +0200

    Added legal move detector

--- src/MoveLoop/P_Move.hs
+++ src/MoveLoop/P_Move.hs
@@ -1,5 +1,13 @@
-module MoveLoop.P_Move () where
+module MoveLoop.P_Move (checkForLegalMove) where
 
-import System.Random ( Random(randomR), StdGen )
+import System.Random ( Random(randomR), StdGen, mkStdGen, newStdGen )
 import Structs(Player)
+import Initialization.Pure.P_initChar(setMarkerEntry)
 
+checkForLegalMove :: (Int,Int) -> [[Int]] -> StdGen -> ([[Int]], Bool)
+checkForLegalMove (_,_) [[]] _ = ([[]],False)
+checkForLegalMove (newX, newY) map seed
+    | newX < 0 || newY < 0 || length map < newX || length map < newY = (map, False)
+    | otherwise = do
+        let newMap = setMarkerEntry (newX, newY) (length map, length map) 88 map [[]]
+        (newMap, True)

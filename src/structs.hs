9ec7139b267c2b1dee0f671186252969676ab737
Axel Jacobsen <aejacobs@stud.ntnu.no>
2022-05-17 01:22:20 +0200

    Added enemy struct

--- src/structs.hs
+++ src/structs.hs
@@ -1,2 +1,4 @@
-module Structs (Player(..)) where         
+module Structs (Player(..),Enemy(..)) where         
 data Player = Player { name::String, maxHp::Int, hp::Int, weapon::Int, armour::Int, healpot::Int, money::Int, lowestLayer::Int, playerPos::(Int,Int), goal::(Int,Int), start::(Int,Int), prevDir::Int} deriving (Show)
+
+data Enemy = Enemy {denotator::String, eName::String, eMaxHp::Int, eHp::Int, eDamage::Int, eArmour::Int, eDrops::Int}

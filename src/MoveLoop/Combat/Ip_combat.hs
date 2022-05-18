87a893218119dde845c381620894c8ae1779f2b1
Axel Jacobsen <aejacobs@stud.ntnu.no>
2022-05-18 18:37:47 +0200

    Added death and combat loot, Closes #9

--- src/MoveLoop/Combat/Ip_combat.hs
+++ src/MoveLoop/Combat/Ip_combat.hs
@@ -2,53 +2,105 @@ module MoveLoop.Combat.Ip_combat where
 import Structs (Player (..), Enemy(..))
 import System.Random ( Random(randomR), StdGen )
 import System.IO ( hGetContents, openFile, IOMode(ReadMode) )
-import MoveLoop.Combat.CombatText (encounterEnemy, coinSuccess, coinNeutral, coinCritical, escape, pAttack1, pAttack2, eAttack, damage)
-import Public.P_updatePlayer (updatePos, newLayer)
+import MoveLoop.Combat.CombatText (killedEnemy1,killedEnemy2,killedEnemy3,killedEnemy4, encounterEnemy, coinSuccess, coinNeutral, coinCritical, escape, pAttack1, pAttack2, eAttack, damage, escapeFail, escapeSuccess, combatOptions1, combatOptions2)
+import TextGeneral(healMessage,outOfHeal)
+import Public.P_updatePlayer (updatePos, newLayer, updateHp, updateHeal,updateMoney, incrementExp)
+import Data.Char (toLower)
+import Public.P_publicFuncs(healPlayer)
+import Public.Ip_publicFuncs(clearConsole)
+import MoveLoop.Combat.P_combat(enemyDamage,playerDamage)
 
 -- I want this to go right into the loop to skip unecessary nests
 -- therefore creature type has to be predecided
-combatLoop :: Player -> Int -> (Int,Int) -> [[Int]] -> (Enemy, StdGen) -> IO (Player, [[Int]], StdGen)
-combatLoop player phase (lEhp, lPhp) unexploredMap (enemy, inSeed) -- lehp and phph are temporary damage counters to reduce function calls
-    | phase == 0 = do-- Enter comabt
+combatLoop :: Player -> Int -> (Int,Int) -> [[Int]] -> (Enemy, StdGen) -> IO (Player, [[Int]], StdGen, Int)
+combatLoop player phase (lEhp, lPhp) dataMap (enemy, inSeed) -- lehp and phph are temporary damage counters to reduce function calls
+    | phase == 0 = do-- Enter combat encounter
         let (coinFlip, outSeed) = randomR (0, 6) inSeed :: (Int, StdGen)
         case coinFlip of
             0 -> do
                 putStrLn (coinCritical++prefix enemy++eName enemy++"!")
-                combatLoop player 2 (lEhp, lPhp) unexploredMap (enemy, inSeed)
+                combatLoop player 2 (lEhp, lPhp) dataMap (enemy, inSeed)
             6 -> do
                 putStrLn (encounterEnemy++prefix enemy++eName enemy)
                 putStrLn (coinSuccess++escape)
-                combatLoop player 3 (lEhp, lPhp) unexploredMap (enemy, inSeed)
+                combatLoop player 8 (lEhp, lPhp) dataMap (enemy, inSeed)
             _ -> do
                 putStrLn (encounterEnemy++prefix enemy++eName enemy)
                 putStrLn coinNeutral
-                combatLoop player 1 (lEhp, lPhp) unexploredMap (enemy, inSeed)
-    | phase == 1 = do       -- Player Turn
+                combatLoop player 1 (lEhp, lPhp) dataMap (enemy, inSeed)
+    | phase == 1 = do   --Player Turn
         printHp (name player) (eName enemy) (maxHp player-lPhp) (eMaxHp enemy-lEhp)
-        combatLoop player 2 (lEhp, lPhp) unexploredMap (enemy, inSeed)
+        putStr combatOptions1
+        putStr (show (healpot player))
+        putStrLn combatOptions2
+        pInput <- getLine
+        if null pInput then combatLoop player 1 (lEhp, lPhp) dataMap (enemy, inSeed)
+        else case toLower (head pInput) of
+            'a' -> do 
+                let (dealtDamage, outSeed) = playerDamage enemy player inSeed
+                clearConsole
+                pDamagePrint player dealtDamage
+                checkDeath player enemy 2 (lEhp+dealtDamage, lPhp) dataMap outSeed
+            'h' -> do
+                if 0 < healpot player then do
+                    let healedPlayer = healPlayer (updateHeal player (-1))
+                    putStrLn (healMessage++ show (hp healedPlayer - hp player) ++"HP.")
+                    combatLoop player 2 (lEhp, lPhp) dataMap (enemy, inSeed)
+                else do 
+                    putStrLn outOfHeal
+                    combatLoop player 1 (lEhp, lPhp) dataMap (enemy, inSeed)
+            'e' ->  combatLoop player 9 (lEhp, lPhp) dataMap (enemy, inSeed)
+            _ -> combatLoop player 1 (lEhp, lPhp) dataMap (enemy, inSeed)
     | phase == 2 = do   --Enemy attack
-        printHp (name player) (eName enemy) (maxHp player-lPhp) (eMaxHp enemy-lEhp)
-        combatLoop player 3 (lEhp, lPhp) unexploredMap (enemy, inSeed)
-    | otherwise = return (player, unexploredMap, inSeed)-- Exit due to error
+        let (takenDamage, outSeed) = enemyDamage enemy player inSeed
+        eDamagePrint enemy takenDamage
+        checkDeath player enemy 1 (lEhp, lPhp+takenDamage) dataMap outSeed
+    | phase == 8 = do   --Free escape
+        putStrLn escape
+        pInput <- getLine
+        if null pInput then combatLoop player 8 (lEhp, lPhp) dataMap (enemy, inSeed)
+        else case toLower (head pInput) of
+            'y' -> return (player, dataMap, inSeed, 0)
+            'n' -> combatLoop player 1 (lEhp, lPhp) dataMap (enemy, inSeed)
+            _ -> combatLoop player 8 (lEhp, lPhp) dataMap (enemy, inSeed)
 
+    | phase == 9 = do   --Attempt escape
+        let (coinFlip, outSeed) = randomR (0, 10) inSeed :: (Int, StdGen)
+        if hp player-lPhp < eMaxHp enemy-lEhp && 3<=coinFlip then do
+            putStrLn escapeFail
+            combatLoop player 2 (lEhp, lPhp) dataMap (enemy, outSeed)
+        else do
+            putStrLn escapeSuccess
+            let escapePlayer = updateHp player lPhp
+            return (escapePlayer, dataMap, outSeed, 0)
+    | phase == 10 = do  --Quit loop, player death
+        printHp (name player) (eName enemy) (maxHp player-lPhp) (eMaxHp enemy-lEhp)
+        return (player, dataMap, inSeed, 1)
+    | phase == 11 = do  --Quit loop, enemy killed
+        printHp (name player) (eName enemy) (maxHp player-lPhp) 0
+        putStrLn (name player++killedEnemy1++eName enemy++killedEnemy2++show (eDrops enemy)++killedEnemy3++show (expDrop enemy)++killedEnemy4)
+        return (updateHp (updateMoney (incrementExp player (expDrop enemy))  (eDrops enemy)) (-lPhp), dataMap, inSeed, 0)
+    | otherwise = return (player, dataMap, inSeed, -1)-- Exit due to error
 
 printHp :: String -> String -> Int -> Int  -> IO ()
 printHp name1 name2 hp1 hp2 = do
     putStr "+"
-    printLine 44
+    printLine 47
     putStr "| "
     putStr (name1++": ")
     printOneUsercontent " " (20-length name1)
     putStr (name2++": ")
     printOneUsercontent " " (20-length name2)
-    printOneUsercontent "|" hp1
-    printOneUsercontent " " (22-hp1)
     putStrLn " "
+    putStr "| "
+    printOneUsercontent "|" hp1
+    printOneUsercontent " " (20-hp1)
     printOneUsercontent "|" hp2
-    printOneUsercontent " " (22-hp2)
+    printOneUsercontent " " (20-hp2)
     putStrLn " "
     putStr "+"
-    printLine 44
+    printLine 47
+
 
 printLine :: Int -> IO ()
 printLine 0 = putStrLn "+"
@@ -56,8 +108,27 @@ printLine count = do
     putStr "-"
     printLine (count-1)
 
+
 printOneUsercontent :: String -> Int -> IO ()
-printOneUsercontent _ 0 = putStr "|"
+printOneUsercontent _ 0 = putStr "| "
 printOneUsercontent symbol count = do
-    putStr symbol
-    printOneUsercontent symbol (count-1)
+    if count < 0 then putStr "| "
+    else do
+        putStr symbol
+        printOneUsercontent symbol (count-1)
+
+
+pDamagePrint :: Player -> Int -> IO ()
+pDamagePrint player damage = putStrLn (name player++" attacks, and deals "++show damage++"!")
+
+
+eDamagePrint :: Enemy -> Int -> IO ()
+eDamagePrint enemy damage = putStrLn("The "++eName enemy++" attacks, and deals "++show damage++"!")
+
+
+--Redirects to next combat stage or to correct death stage if someone died
+checkDeath :: Player -> Enemy -> Int -> (Int,Int) -> [[Int]] -> StdGen -> IO (Player, [[Int]], StdGen, Int)
+checkDeath player enemy nextPhase (lEhp, lPhp) dataMap passSeed
+    | hp player - lPhp <= 0 = combatLoop player 10 (lEhp, lPhp) dataMap (enemy, passSeed)
+    | eMaxHp enemy - lEhp <= 0 = combatLoop player 11 (lEhp, lPhp) dataMap (enemy, passSeed)
+    | otherwise = combatLoop player nextPhase (lEhp, lPhp) dataMap (enemy, passSeed)
--- src/MoveLoop/Combat/P_combat.hs
+++ src/MoveLoop/Combat/P_combat.hs
@@ -1,6 +1,6 @@
-module MoveLoop.Combat.P_combat(generateEnemy) where
+module MoveLoop.Combat.P_combat(generateEnemy,enemyDamage,playerDamage) where
 
-import Structs(Enemy(..))
+import Structs(Player(..), Enemy(..))
 import System.Random (Random(randomR), StdGen)
 
 
@@ -8,7 +8,7 @@ generateEnemy :: StdGen -> Int -> (Enemy, StdGen)
 generateEnemy inSeed typeCap = do
         let (enemyType, strengthSeed) = randomR (0, typeCap) inSeed :: (Int, StdGen) -- Generate enemy
         let (enemyStrength, outSeed) = randomR (0, 4) strengthSeed :: (Int, StdGen) -- Generate enemy strength
-        ((generateEnemyInner enemyType enemyStrength),outSeed)
+        (generateEnemyInner enemyType enemyStrength,outSeed)
 
 generateEnemyInner :: Int -> Int -> Enemy
 generateEnemyInner enemyType statRng    --Enemy type will always be between 0 - and current player layer depth
@@ -22,24 +22,40 @@ generateEnemyInner enemyType statRng    --Enemy type will always be between 0 - 
 --Generates a slime type enemy
 genSlime :: Int -> Enemy
 genSlime 0 = genSlime 1
-genSlime rng = Enemy{prefix = "a ", eName = "Slime", eMaxHp = (3*rng), eDamage = (rng), eArmour = (rng-1), eDrops = (rng), expDrop = 1}
+genSlime rng = Enemy{prefix = "a ", eName = "Slime", eMaxHp = 3*rng, eDamage = rng, eArmour = rng-1, eDrops = rng, expDrop = 1}
 
 --Generates a goblin type enemy
 genGoblin :: Int -> Enemy
 genGoblin 0 = genGoblin 1
-genGoblin rng = Enemy{prefix = "a ", eName = "Goblin", eMaxHp = (2*rng), eDamage = (2*rng), eArmour = (rng+1), eDrops = (rng+2), expDrop = 2}
+genGoblin rng = Enemy{prefix = "a ", eName = "Goblin", eMaxHp = 2*rng, eDamage = 2*rng, eArmour = rng+1, eDrops = rng+2, expDrop = 2}
 
 --Generates an Orc type enemy
 genOrc :: Int -> Enemy
 genOrc 0 = genOrc 1
-genOrc rng = Enemy{prefix = "an ", eName = "Orc", eMaxHp = (6*rng), eDamage = (3*rng), eArmour = (rng*2), eDrops = (rng*5), expDrop = 5}
+genOrc rng = Enemy{prefix = "an ", eName = "Orc", eMaxHp = 6*rng, eDamage = 3*rng, eArmour = rng*2, eDrops = rng*5, expDrop = 5}
 
 -- Generates a drake type enemy
 genDrake :: Int -> Enemy
 genDrake 0 = genDrake 1
-genDrake rng = Enemy{prefix = "a ", eName = "Drake", eMaxHp = (16*rng), eDamage = (6*rng), eArmour = (8*rng), eDrops = (rng*20), expDrop = 10}
+genDrake rng = Enemy{prefix = "a ", eName = "Drake", eMaxHp = 16*rng, eDamage = 6*rng, eArmour = 8*rng, eDrops = rng*20, expDrop = 10}
 
 -- Generates an abomination
 genAbomination :: Int -> Enemy
 genAbomination 0 = genAbomination 1
-genAbomination rng = Enemy{prefix = "an ", eName = "Abomination", eMaxHp = (30*rng), eDamage = (12*rng), eArmour = (10*rng), eDrops = (rng*100), expDrop = 20}
+genAbomination rng = Enemy{prefix = "an ", eName = "Abomination", eMaxHp = 30*rng, eDamage = 12*rng, eArmour = 10*rng, eDrops = rng*100, expDrop = 20}
+
+enemyDamage :: Enemy -> Player -> StdGen -> (Int, StdGen)
+enemyDamage enemy player inSeed = do
+    let (hitReg, outSeed) = randomR (0, 10) inSeed :: (Int, StdGen) -- Check hit
+    case hitReg of
+        0 -> (0, outSeed)
+        10 -> ((eDamage enemy*2)-armour player, outSeed)
+        _ -> (eDamage enemy-armour player, outSeed)
+
+playerDamage :: Enemy -> Player -> StdGen -> (Int, StdGen)
+playerDamage enemy player inSeed = do
+    let (hitReg, outSeed) = randomR (0, 10) inSeed :: (Int, StdGen) -- Check hit
+    case hitReg of
+        0 -> (0, outSeed)
+        10 -> ((weapon player*2)-eArmour enemy, outSeed)
+        _ -> (weapon player-eArmour enemy, outSeed)

e74bae655ac00b2fae1668279e2cd64ae2d3adaf
Axel Jacobsen <aejacobs@stud.ntnu.no>
2022-05-20 00:02:59 +0200

    implemented "--help" flag with legal variants

--- app/Main.hs
+++ app/Main.hs
@@ -6,10 +6,16 @@ import System.Random (mkStdGen, newStdGen)
 import Structs (Player)
 import MainLoop.Ip_mainLoop (gameLoop)
 import System.IO
-import TextGeneral(pressEnter)
+import TextGeneral(pressEnter, helpFlag1, helpFlag2, helpFlag3, continue)
+import System.Directory.Internal.Prelude (getArgs)
+import Data.Char(toLower)
 
 main :: IO ()
 main = do
+    needHelp <- getArgs
+    let acceptAbleOptions = ["--help","-help","help","--h","-h","h"]
+    let getHelp = if not (null needHelp) && (map toLower (head needHelp) `elem` acceptAbleOptions) then do putStrLn (helpFlag1++helpFlag2++helpFlag3) ; putStrLn continue ; _ <- getLine; putStr "" else putStr ""
+    getHelp
     originalSeed <- newStdGen
     contents <- readFile "logo.txt"
     putStrLn contents

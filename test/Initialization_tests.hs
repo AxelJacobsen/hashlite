0a4639a4b81a608015d9b740ee48490d58438efc
Axel Jacobsen <aejacobs@stud.ntnu.no>
2022-05-20 18:58:07 +0200

    Added testing for Initialization functions

---
+++ test/Initialization_tests.hs
@@ -0,0 +1,48 @@
+module Initialization_tests (initTests) where
+import Test.QuickCheck
+import Control.Exception (evaluate)
+import Public.Consts.Structs (Player (..))
+import System.Random ( Random(randomR), StdGen, mkStdGen )
+import Initialization.Pure.P_initChar (calcLower, generateCharacter, placeStartEnd, setMarkerEntry,setMarkerInner)
+import Test.HUnit
+
+testSeed = mkStdGen 10
+
+playerTest1 = Player {name = "Tormod",maxHp=8, hp=8, weapon  = 3, armour  = 2, healpot = 3, money   = 10, lowestLayer = 0, playerPos = (0,0), start = (0,0), goal = (0,0), prevDir = 0, pExp = 0, levelCap = 1}
+playerTest2 = Player {name = "Jostein",maxHp=8, hp=8, weapon  = 3, armour  = 2, healpot = 3, money   = 10, lowestLayer = 0, playerPos = (0,0), start = (0,0), goal = (0,0), prevDir = 0, pExp = 0, levelCap = 1}
+
+startEndTestMap1 = [[0,0,0],[0,0,0],[0,0,0]]
+startEndResultMap1 = [[0,99,0],[0,0,0],[0,0,0]]
+setMarkEntryResultMap = [[0,0,0],[0,0,0],[0,99,0]]
+serMarkInnerResultMap = [0,99,0]
+
+initTests :: Test
+initTests = TestList [
+  TestLabel "generateCharacter" $
+    TestCase $ assertEqual "creates a new character with given name"
+    (generateCharacter "Tormod")
+    playerTest1,
+    TestCase $ assertEqual "creates character with empty name"
+    (generateCharacter "")
+    playerTest2,
+  
+  TestLabel "placeStartEnd" $
+    TestCase $ assertEqual "places Start and end randomly on a map"
+    (placeStartEnd (length startEndTestMap1) True (startEndTestMap1, testSeed))
+    (startEndResultMap1,testSeed,2,1),
+  
+  TestLabel "setMarkerEntry" $
+    TestCase $ assertEqual "places a marker in a spot on a 2d map"
+    (setMarkerEntry (1,1) (length setMarkEntryResultMap, length setMarkEntryResultMap) 99 startEndTestMap1 [[]])
+    setMarkEntryResultMap,
+  
+  TestLabel "setMarkerInner" $
+    TestCase $ assertEqual "places a marker in a spot on a 1d list"
+    (setMarkerInner 1 (length serMarkInnerResultMap) 99 [] (head startEndTestMap1))
+    serMarkInnerResultMap,
+  
+  TestLabel "calcLower" $
+    TestCase $ assertEqual "Gets lower rng limit for startPos"
+    (calcLower 10 0.7)
+    7
+  ]
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,21 @@
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+import Public.Consts.Structs (Player (..))
+import System.Random ( Random(randomR), StdGen )
+import System.Directory(doesFileExist)
+import MainLoop.P_mainLoop(checkLegalIdleChoice)
+import Initialization.Pure.P_initChar (generateCharacter, placeStartEnd)
+import Initialization.Pure.P_MapGenerator (generateBoard, generateEmptyBoard)
+import MoveLoop.P_Move(checkForLegalMove)
+import MoveLoop.Combat.P_combat(generateEnemy, genMimic)
+import Public.P_updatePlayer (incrementExp, updatePos, newLayer)
+import Public.P_publicFuncs (healPlayer)
+
+import Initialization_tests(initTests)
+import Test.HUnit (runTestTT)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    _ <- runTestTT initTests
+    putStrLn "Passed all tests!"
